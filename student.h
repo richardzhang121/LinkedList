defe1fcb1e170a33f618cf8678eab701be659f56
richardzhang121 <richard.zhang121@gmail.com>
2017-02-03 19:47:38 -0800

    Update student.h

--- student.h
+++ student.h
@@ -1,15 +1,23 @@
 #ifndef STUDENT_H
 #define STUDENT_H
+
 #include <iostream>
+#include <string.h>
 
-using namespace std;
 
+//student class with four variables (gpa, id, first name, last name)
 class Student{
-    public:
-        Student(int);
-        int getID();
-    protected:
-        int ID;
+ public:
+  Student();
+  ~Student();
+  void printStudent();
+  int getId();
+  float getGpa();
+ private:
+  char firstName[80];
+  char lastName[80];
+  int id;
+  float gpa;
 };
 
 #endif
