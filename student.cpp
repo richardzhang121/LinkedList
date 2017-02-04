de12d3266878ec6cc83380df8c5047586df5b362
richardzhang121 <richard.zhang121@gmail.com>
2017-02-03 19:47:20 -0800

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,13 +1,42 @@
-#include "student.h"
 #include <iostream>
 #include <cstring>
+#include <iomanip>
+#include "student.h"
 
 using namespace std;
 
-Student::Student(int newID){
-    ID = newID;
+//constructor
+Student::~Student(){
+
+}
+//deconstructor
+Student::Student(){
+  cout << "What is the first name?" << endl;
+  cin.get(firstName,30);
+  cin.ignore();
+  cout << "What is the last Name" << endl;
+  cin.get(lastName,30);
+  cin.ignore();
+  cout << "What is the ID" << endl;
+  cin >> id;
+  cin.ignore();
+  cout << "What is the GPA?" << endl;
+  cin >> gpa;
+  cin.ignore();
+  cout << "Name: " << firstName << " " << lastName << ", ID: " << id << ", GPA: " << setprecision(2) <<fixed << gpa << endl;
 }
 
-int Student::getID(){
-    return ID;
+//gets ID
+int Student::getId(){
+  return id;
+}
+
+//gets GPA
+float Student::getGpa(){
+  return gpa;
+}
+
+//prints student
+void Student::printStudent() {
+  cout << "Student: " << firstName << " " << lastName << ", " << id << ", " << gpa << endl;
 }
